92a00f00c97f31529f04b5d1786a2965961c119f
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2020-08-05 10:33:07 +0900

    AddSeekBar

--- app/src/main/java/com/example/easy_written/KeywordTime.java
+++ app/src/main/java/com/example/easy_written/KeywordTime.java
@@ -1,9 +1,11 @@
 package com.example.easy_written;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
@@ -11,23 +13,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class KeywordTime extends Fragment {
     private View view;
-    private String KeywordTimeFileName,KeywordTimeFileDate,KeywordTimeFilePath, AudioPath;
+    private String KeywordTimeFileName,KeywordTimeFileDate,KeywordTimeFilePath, iwantplayaudio;
     private Bundle KeywordTimeBundle;
-    private int PlayAndCancelCheck=1;
+    public boolean PlayAndCancelCheck=true, isPause=false;
     private MediaPlayer mediaPlayer;
     private MediaRecorder mediaRecorder;
     private String[] SplitKeyName;
+    private static TextView setSTTString;
+    private String STT;
+    private int AudioDuration;
+    private SeekBar AudioSeekBar;
 
     //newInstance
     public static KeywordTime newInstance(){
@@ -35,10 +47,21 @@ public class KeywordTime extends Fragment {
         return keywordtime;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("create","create");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view= inflater.inflate(R.layout.activity_keyword_time,container,false);
+
+        Image_MainAdpater image_mainAdpater=new Image_MainAdpater();
+        STT=image_mainAdpater.counter();
+        String STT2=STT+"/"+"STTtext.txt";
+        iwantplayaudio=STT+"/"+ "_audio_record"+".3gp";
 
         //데이터 전달받기
         KeywordTimeBundle = getArguments();
@@ -46,38 +69,30 @@ public class KeywordTime extends Fragment {
             KeywordTimeFileName = KeywordTimeBundle.getString("keyFileNamedata");
             KeywordTimeFileDate = KeywordTimeBundle.getString("keyFileDatedata");
             KeywordTimeFilePath = KeywordTimeBundle.getString("keyPathdata");
-//            SplitKeyName=KeywordTimeFilePath.split("#");
-            AudioPath=KeywordTimeFilePath+"/"+ "_audio_record"+".3gp";
-
-            Log.e("KeywordTimeFileName!!~~ :",KeywordTimeFileName);
-            Log.e("KeywordTimeFileDate!!~~ :",KeywordTimeFileDate);
-            Log.e("KeywordTimeFilePath!!~~ :",KeywordTimeFilePath);
-            Log.e("AudioPath->",AudioPath);
-            Log.e("Environment",Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +"EASYWRITTEN"+ "/"); }
+        }
         else{
         }
 
         SpannableString content1 = new SpannableString("안드로이드");
         content1.setSpan(new UnderlineSpan(), 0, content1.length(), 0);
 
-        TextView Tandroid1=view.findViewById(R.id.Tandroid1);
-        Tandroid1.setText(content1);
-        TextView Tandroid2=view.findViewById(R.id.Tandroid2);
-        Tandroid2.setText(content1);
-        TextView Tandroid3=view.findViewById(R.id.Tandroid3);
-        Tandroid3.setText(content1);
+        String STTtextToString=ReadTextFile(STT2);
+        TextView textView=view.findViewById(R.id.setSTTTExtView);
+        textView.setText(STTtextToString);
 
-        Button AudioRunButton=view.findViewById(R.id.AudioRunButton);
+        ImageButton AudioRunButton=view.findViewById(R.id.AudioRunButton);
         AudioRunButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PlayAndCancel();
+                PlayAndCancel(iwantplayaudio);
             }
         });
 
-        int value=30;
-        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progressBar) ;
-        progressBar.setProgress(value) ;
+
+        AudioSeekBar = (SeekBar) view.findViewById(R.id.AudioSeekBar) ;
+
+        ViewPageAdapter viewPageAdapter=new ViewPageAdapter(getFragmentManager());
+        viewPageAdapter.notifyDataSetChanged();
 
         return view;
     }
@@ -87,18 +102,17 @@ public class KeywordTime extends Fragment {
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        mediaRecorder.setOutputFile(AudioPath);
     }
 
-    public void PlayAndCancel(){
-        if(PlayAndCancelCheck==1) {
+    private void PlayAndCancel(String path){
+        if(PlayAndCancelCheck==true) {
             mediaPlayer = new MediaPlayer();
+
             try {
-                Log.i("AudioPathdata",AudioPath);
-                mediaPlayer.setDataSource(AudioPath);
-                //mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                Log.e("녹음파일 재생","재생");
+                mediaPlayer.setDataSource(path);
                 mediaPlayer.prepare();
-                mediaPlayer.start();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }catch(NullPointerException e){
@@ -106,20 +120,64 @@ public class KeywordTime extends Fragment {
             }catch (IllegalStateException e){
                 e.printStackTrace();
             }
-
-            PlayAndCancelCheck=0;
+            AudioDuration=mediaPlayer.getDuration();
+            AudioSeekBar.setMax(AudioDuration);
+            AudioSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if(fromUser)
+                        mediaPlayer.seekTo(progress);
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+            mediaPlayer.start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(mediaPlayer.isPlaying()){
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        AudioSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                }
+            }).start();
+            PlayAndCancelCheck=false;
         }
-        else if(PlayAndCancelCheck==0){
+        else if(PlayAndCancelCheck==false){
+            Log.e("녹음파일 재생 중지","중지");
             if (mediaPlayer != null) {
+                isPause=true;
                 mediaPlayer.pause();
-                mediaPlayer.stop();
-                mediaPlayer.release();
-                setupMediaRecorder();
+//                mediaPlayer.stop();
+//                mediaPlayer.release();
+//                setupMediaRecorder();
             }
-            PlayAndCancelCheck=1;
+            PlayAndCancelCheck=true;
         }
     }
 
-
-
+    //경로의 텍스트 파일읽기
+    public String ReadTextFile(String path){
+        StringBuffer strBuffer = new StringBuffer();
+        try{
+            InputStream is = new FileInputStream(path);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line="";
+            while((line=reader.readLine())!=null){
+                strBuffer.append(line+"\n");
+            }
+            reader.close();
+            is.close();
+        }catch (IOException e){
+            e.printStackTrace();
+            return "";
+        }
+        return strBuffer.toString();
+    }
 }
